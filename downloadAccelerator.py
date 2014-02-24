b7d916a59f091f087c806d5b5824f604bd75d1c6
Edward Ekstrom <edward.ekstrom@gmail.com>
2014-02-23 22:52:04 -0700

    set up timing and correct print out

--- downloadAccelerator.py
+++ downloadAccelerator.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import requests
 import threading
+import time
 from fileinput import filename
 
 ''' Downloader for a set of files '''
@@ -17,7 +18,7 @@ class Downloader:
     def parse_arguments(self):
         ''' parse arguments, which include file '-d' for download directory'''
         parser = argparse.ArgumentParser(prog='Mass downloader', description='A simple script that downloads multiple files from a list of URLs specified in a file', add_help=True)
-        parser.add_argument('url', type=str, action='store', help='The URL to download',default='http://174.52.164.54/files/design-philosophy-sigcomm-88.pdf')
+        parser.add_argument('url', type=str, action='store', help='The URL to download',default='http://174.52.164.54/files/design-philosophy-sigcomm-88.pdf', nargs='?')
         parser.add_argument('-n', '--threads', type=int, action='store', help='Specify the number of threads used to download the file.',default=10)
         args = parser.parse_args()
         self.url = args.url
@@ -38,7 +39,7 @@ class Downloader:
         # get header content-length
         r = requests.head(self.url, stream=True)
         headers = r.headers
-        print headers['content-length']
+        #print headers['content-length']
         contentLength = int(headers['content-length'])
         
         curByte = 0
@@ -47,6 +48,7 @@ class Downloader:
         sharedDictionary = {}
         # create a thread for each url
         threads = []
+        
         for x in range(0, self.threads - 1):
             d= DownThread(self.url,curByte,curByte + nth,sharedDictionary,x)
             threads.append(d)
@@ -59,13 +61,16 @@ class Downloader:
             filename = self.dir + '/' + f
             d = DownThread(url,filename)
             threads.append(d)'''
+        start_time = time.time()
         for t in threads:
             t.start()
         for t in threads:
             t.join()
+        end_time = time.time()
         with open(file, 'wb') as f:
             for x in range(0, self.threads):
                 f.write(sharedDictionary[x])
+        print self.url + ' ' + str(self.threads) + ' ' + str(contentLength) + ' ' + "%g" % (end_time - start_time)
 
 ''' Use a thread to download part of a given file'''
 class DownThread(threading.Thread):
@@ -73,7 +78,7 @@ class DownThread(threading.Thread):
         self.url = url
         self.startByte = str(startByte)
         self.endByte = str(endByte)
-        print str(index) + ': ' + self.startByte + ' - ' + self.endByte + '\n'
+        #print str(index) + ': ' + self.startByte + ' - ' + self.endByte
         self.sharedDictionary = sharedDict
         self.index = index
         #print str(startByte) + ' - ' + str(endByte)
@@ -84,7 +89,7 @@ class DownThread(threading.Thread):
         #print 'Downloading %s' % self.url
         r = requests.get(self.url, stream=True, headers={'Range': 'bytes={0}-{1}'.format(self.startByte,self.endByte),'accept-encoding': ''})
         #print r.content
-        print r
+        #print r
         self.sharedDictionary[self.index] = r.content
         #with open(self.filename, 'wb') as f:
          #   f.write(r.content)
